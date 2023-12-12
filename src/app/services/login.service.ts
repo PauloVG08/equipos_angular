948910f0b5559d4035ccfab443ccf650aa001238
Paulo <paulo081002@gmail.com>
2023-12-12 08:49:30 -0600

    cambios

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,14 +6,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  private isAuthenticated: boolean = true;
+  private isAuthenticated: boolean = false;
+
+  constructor() {
+    // Recuperar el estado de autenticación al iniciar el servicio
+    this.isAuthenticated = this.retrieveAuthenticationState();
+  }
+
+  private retrieveAuthenticationState(): boolean {
+    // Intentar recuperar el estado de autenticación desde LocalStorage
+    const storedState = localStorage.getItem('isAuthenticated');
+    return storedState ? JSON.parse(storedState) : false;
+  }
+
+  private updateAuthenticationState(state: boolean): void {
+    // Actualizar el estado de autenticación en LocalStorage
+    localStorage.setItem('isAuthenticated', JSON.stringify(state));
+  }
 
   login() {
     this.isAuthenticated = true;
+    this.updateAuthenticationState(true);
   }
 
   logout() {
     this.isAuthenticated = false;
+    this.updateAuthenticationState(false);
   }
 
   isLoggedIn(): boolean {
